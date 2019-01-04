39ff8aafd73ef9023c5e129424c4685cab4ff0c9
Elendin37 <46263337+Elendin37@users.noreply.github.com>
2019-01-04 16:41:40 +0100

    Änderung Albert

--- app/src/main/java/com/example/tomr/edutracker_1/Timer.java
+++ app/src/main/java/com/example/tomr/edutracker_1/Timer.java
@@ -7,20 +7,28 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Timer extends AppCompatActivity {
+public class Timer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     public static final long SLEEPTIME = 10;
     boolean running;
@@ -32,11 +40,15 @@ public class Timer extends AppCompatActivity {
     Calendar currentStartTime, currentStopTime;
     AudioManager am;
     int oldRingerMode;
+    //insert this for toolbar:
+    public android.support.v7.widget.Toolbar toolbar;
+    private ArrayList<String> values;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timer);
+        setContentView(R.layout.main_timer);
         running = false;
         time = 0;
         start =  findViewById(R.id.b_starttimer);
@@ -46,6 +58,27 @@ public class Timer extends AppCompatActivity {
 
         currentStartTime = Calendar.getInstance();//.getTime();
 
+
+        //----------------Toolbar + Application Drawer begin --------------------------------------//
+        // initialize Toolbar: (needed for the app.Drawer)
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //initialize the Drawer:
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //Werte für Adapter anlegen
+        values = new ArrayList<String>();
+
+        //Adapter anlegen
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+        //----------------Toolbar + Application Drawer end-----------------------------------------//
         /*
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle("Stoppuhr");
@@ -176,5 +209,55 @@ public class Timer extends AppCompatActivity {
         });
         refreshThread.start();
     }
+    //----------------Toolbar + Application Drawer-------------------------------------------------//
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_tracking) {
+            // Handle the camera action
+        } else if (id == R.id.nav_lerneinheiten) {
+
+        } else if (id == R.id.nav_statistik) {
+
+        } else if (id == R.id.nav_errungenschaften) {
+            Intent goals = new Intent(getApplicationContext(),MainActivityReward.class);
+            startActivity(goals);
+
+        } else if (id == R.id.nav_manage) {
+            Intent manage = new Intent(getApplicationContext(),Einstellung_Uebersicht.class);
+            startActivity(manage);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    //----------------Toolbar + Application Drawer--------------------------------------------//
 
 }
