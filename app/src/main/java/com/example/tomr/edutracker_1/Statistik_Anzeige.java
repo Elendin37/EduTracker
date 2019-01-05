3858993e11103071189e68b6252ffca61f2d9a55
christoph711 <christoph.ludwig93@gmx.de>
2019-01-05 11:37:31 +0100

    Update1.2: Oberfläche und Funktionen für die Statistik (Fehlerabfangen)

--- app/src/main/java/com/example/tomr/edutracker_1/Statistik_Anzeige.java
+++ app/src/main/java/com/example/tomr/edutracker_1/Statistik_Anzeige.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.text.ParseException;
@@ -16,6 +17,7 @@ public class Statistik_Anzeige extends AppCompatActivity {
     MyDatabaseHelper db = new MyDatabaseHelper(this, null, null, 0);
 
     TextView tvTest;
+    TextView tvdatum;
     ArrayList<String> FächerList=new ArrayList<>();
 
     boolean alleFächer = false;
@@ -26,12 +28,14 @@ public class Statistik_Anzeige extends AppCompatActivity {
         setContentView(R.layout.activity_statistik_anzeige);
 
         tvTest = (TextView)findViewById(R.id.tv_test);
-
+        tvdatum = (TextView)findViewById(R.id.tv_datum);
         String ausgabe = "";
 
         FächerList = getIntent().getStringArrayListExtra("ÜbergabeFächer");
         String Startdatum = getIntent().getExtras().getString("ÜbergabeStartdatum");
         String Enddatum = getIntent().getExtras().getString("ÜbergabeEnddatum");
+
+        tvdatum.setText(Startdatum + " - " + Enddatum);
 
         Integer anzahlfächer = FächerList.size();
 
@@ -59,13 +63,16 @@ public class Statistik_Anzeige extends AppCompatActivity {
             }
         }
 
-        tvTest.setText(ausgabe+"\n"+Startdatum+"\n"+Enddatum+"\n");
 
+        ausgabe = ausgabe + "\n\nAngabe in [hh:mm:ss]";
 
-
+        tvTest.setText(ausgabe);
     }
 
-
+    public void onClick_b_fertig(View view){
+        Intent StatistikAuswahl = new Intent(this, Statistik_Auswahl.class);
+        startActivity(StatistikAuswahl);
+    }
 
 
 }
